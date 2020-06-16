b7e6d7a072a2f6b64d9e9fdc0d9eaad5282d3509
Duván Villa <38322609+elgatoudea@users.noreply.github.com>
2020-06-15 20:25:07 -0500

    Se crean 25 item mas para mostrar en pantall

--- app.js
+++ app.js
@@ -72,6 +72,57 @@ console.log(cont);
     var intelecP = JSON.stringify(x.personality[0].children[4].percentile, null, 2);
     var desafAut = JSON.stringify(x.personality[0].children[5].name, null, 2);
     var desafAutP= JSON.stringify(x.personality[0].children[5].percentile, null, 2);
+    var necex = JSON.stringify(x.personality[1].children[0].name,null,2);
+    var necexP = JSON.stringify(x.personality[1].children[0].percentile,null,2);
+    var caut = JSON.stringify(x.personality[1].children[1].name,null,2);
+    var cautP = JSON.stringify(x.personality[1].children[1].percentile,null,2);
+    var ob = JSON.stringify( x.personality[1].children[2].name, null, 2);
+    var obP = JSON.stringify( x.personality[1].children[2].percentile, null, 2);
+    var dis = JSON.stringify( x.personality[1].children[3].name, null, 2);
+    var disP = JSON.stringify( x.personality[1].children[3].percentile, null, 2);
+    var autodis = JSON.stringify( x.personality[1].children[4].name, null, 2);
+    var autodisP = JSON.stringify( x.personality[1].children[4].percentile, null, 2);
+    var autoEf = JSON.stringify( x.personality[1].children[5].name, null, 2);
+    var autoEfP = JSON.stringify( x.personality[1].children[5].percentile, null, 2);
+    var act = JSON.stringify( x.personality[2].children[0].name, null, 2);
+    var actP = JSON.stringify( x.personality[2].children[0].percentile, null, 2);
+    var seg = JSON.stringify( x.personality[2].children[1].name, null, 2);
+    var segP = JSON.stringify( x.personality[2].children[1].percentile, null, 2);
+    var aleg = JSON.stringify( x.personality[2].children[2].name, null, 2);
+    var alegP = JSON.stringify( x.personality[2].children[2].percentile, null, 2);
+    var busqem = JSON.stringify( x.personality[2].children[3].name, null, 2);
+    var busqemP = JSON.stringify( x.personality[2].children[3].percentile, null, 2);
+    var sim = JSON.stringify( x.personality[2].children[4].name, null, 2);
+    var simP = JSON.stringify( x.personality[2].children[4].percentile, null, 2);
+    var sociab = JSON.stringify( x.personality[2].children[5].name, null, 2);
+    var sociabP = JSON.stringify( x.personality[2].children[5].percentile, null, 2);
+    var altru = JSON.stringify( x.personality[3].children[0].name, null, 2);
+    var altruP = JSON.stringify( x.personality[3].children[0].percentile, null, 2);
+    var coo = JSON.stringify( x.personality[3].children[1].name, null, 2);
+    var cooP = JSON.stringify( x.personality[3].children[1].percentile, null, 2);
+    var modes = JSON.stringify( x.personality[3].children[2].name, null, 2);
+    var modesP = JSON.stringify( x.personality[3].children[2].percentile, null, 2);
+    var intransi = JSON.stringify( x.personality[3].children[3].name, null, 2);
+    var intransiP = JSON.stringify( x.personality[3].children[3].percentile, null, 2);
+    var compas = JSON.stringify( x.personality[3].children[4].name, null, 2);
+    var compasP = JSON.stringify( x.personality[3].children[4].percentile, null, 2);
+    var confianzab = JSON.stringify( x.personality[3].children[5].name, null, 2);
+    var confianzaP = JSON.stringify( x.personality[3].children[5].percentile, null, 2);
+    var vehem = JSON.stringify( x.personality[4].children[0].name, null, 2);
+    var vehemP = JSON.stringify( x.personality[4].children[0].percentile, null, 2);
+    var preoc = JSON.stringify( x.personality[4].children[1].name, null, 2);
+    var preocP = JSON.stringify( x.personality[4].children[1].percentile, null, 2);
+    var mel = JSON.stringify( x.personality[4].children[2].name, null, 2);
+    var melP = JSON.stringify( x.personality[4].children[2].percentile, null, 2);
+    var desm = JSON.stringify( x.personality[4].children[3].name, null, 2);
+    var desmP = JSON.stringify( x.personality[4].children[3].percentile, null, 2);
+    var tim = JSON.stringify( x.personality[4].children[4].name, null, 2);
+    var timP = JSON.stringify( x.personality[4].children[4].percentile, null, 2);
+    var susTen = JSON.stringify( x.personality[4].children[5].name, null, 2);
+    var susTenP = JSON.stringify( x.personality[4].children[5].percentile, null, 2);
+  
+
+
     var y=Estructurador.cleansing(x);
     console.log(y);
 
@@ -88,7 +139,57 @@ console.log(cont);
   inte:intelec,
   intep:intelecP,
   desa:desafAut,
-  desap:desafAutP
+  desap:desafAutP,
+  nec:necex,
+  necP:necexP,
+  cau:caut,
+  cauP:cautP,
+  o: ob,
+   ob1:  obP ,
+   di:  dis ,
+   dis1:  disP ,
+   autodi:  autodis ,
+   autodis1:  autodisP ,
+   autoE:  autoEf ,
+   autoEf1:  autoEfP ,
+   ac:  act ,
+   act1:  actP ,
+   se:  seg ,
+   seg1:  segP ,
+   ale:  aleg ,
+   aleg1:  alegP ,
+   busqe:  busqem ,
+   busqem1:  busqemP ,
+   si:  sim ,
+   sim1:  simP ,
+   socia:  sociab ,
+   sociab1:  sociabP ,
+   altr:  altru ,
+   altru1:  altruP ,
+   co:  coo ,
+   coo1:  cooP ,
+   mode:  modes ,
+   modes1:  modesP ,
+   intrans:  intransi ,
+   intransi1:  intransiP ,
+   compa:  compas ,
+   compas1:  compasP ,
+   confianza:  confianzab ,
+   confianza1:  confianzaP ,
+   vehe:  vehem ,
+   vehem1:  vehemP ,
+   preo:  preoc ,
+   preoc1:  preocP ,
+   me:  mel ,
+   mel1:  melP ,
+   des:  desm ,
+   desm1:  desmP ,
+   ti:  tim ,
+   tim1:  timP ,
+   susTe:  susTen ,
+   susTen1:  susTenP
+
+
 
     });//este es el view que se carga
 });
